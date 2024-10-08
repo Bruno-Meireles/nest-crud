7b1f120022b4669d0c8608aefb8f5b32e927e47f
Bruno Meireles <bruno.meirelessilva90@gmail.com>
2024-10-07 21:04:43 -0300

    Removendo userList

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,12 +1,10 @@
 import UserForm from "./components/UserForm";
-import UserList from "./components/UserList";
 
 const App = () => {
   return (
     <div>
       <h1>Gerenciamento de Usuários</h1>
       <UserForm />
-      <UserList />
     </div>
   );
 };
